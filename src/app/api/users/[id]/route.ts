62aae52fc31957330c485140940b7083cb7445ed
Ruan Carlos <ruan.nascimento019@gmail.com>
2025-03-21 19:58:41 -0300

    fix: correction in api users

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: any }) {
   try {
     const user = await prisma.user.findUnique({
       where: { id: params.id },
